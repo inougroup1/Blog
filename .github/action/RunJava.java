3796792e4ad59a5bd36e17d344ad31f8f45ba5c6
inougroup1 <78524039+inougroup1@users.noreply.github.com>
2021-02-16 17:39:21 +0900

    Update RunJava.java

--- .github/action/RunJava.java
+++ .github/action/RunJava.java
@@ -1,7 +1,28 @@
+import java.io.File;
+
 public class RunJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java action");
 	}
+	
+	private static void walk( String path ) {
 
+        File root = new File( path );
+        File[] list = root.listFiles();
+
+        if (list == null) {
+        	return;
+        }
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                walk( f.getAbsolutePath() );
+            }
+            else {
+            	String fullPath = f.getAbsoluteFile().toString();
+                System.out.println(fullPath);
+            }
+        }
+    }
 }
