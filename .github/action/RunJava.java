0414ad61899658c371f120fd3e5783b0ec6364ea
inougroup1 <78524039+inougroup1@users.noreply.github.com>
2021-02-16 18:26:51 +0900

    Update RunJava.java

--- .github/action/RunJava.java
+++ .github/action/RunJava.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class RunJava {
-
 	public static void main(String[] args) {
 		System.out.println("Hello walk action");
 		walk("/home/runner/work/blog/blog/./docs/");
@@ -25,8 +30,48 @@ public class RunJava {
             	String fileName = f.getName();
             	if(fileName.contains("index.html")) {
             		System.out.println(fullPath);
+            		changeTxt(fullPath);
             	}
             }
         }
     }
+	
+	private static void changeTxt( String path ) {
+		try {
+			boolean isChanged = false;
+			
+			StringBuilder sb = new StringBuilder();
+	        File file = new File(path);
+	        FileInputStream fis = new FileInputStream(file);
+	        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+	        BufferedReader br = new BufferedReader(isr);
+	        String line = "";
+	        
+	        while((line = br.readLine()) != null){
+	            if(line.contains("test01")) {
+	            	isChanged = true;
+	            	line = line.replace("test01", "taaa01");
+	            }
+	        	sb.append(line);
+	            sb.append(System.getProperty("line.separator"));
+	        }
+	        
+	        br.close();
+	        
+	        if(isChanged) {
+	        	System.out.println("changed");
+	        	file.delete();
+        		file.createNewFile();
+
+    			BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file.getPath()), "UTF8"));
+
+    			output.write(sb.toString());
+    			output.close();
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+
 }
